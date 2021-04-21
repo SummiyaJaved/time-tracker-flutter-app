f82e0cf089f178a970dec3d4acd8afbbe64e926e
ahteshamulhaq243 <ahteshamawan207@gmail.com>
2021-04-22 02:20:39 +0500

    Following things are done - Did some practice on Column Widget - Added 3 Containers with Orange, Red and Purple Color - Added height of 100.0 px inside the 3 containers - Set crossAxisAlignment to stretch - Made the scaffold body a separate private method

--- lib/app/signin/sign_in_page.dart
+++ lib/app/signin/sign_in_page.dart
@@ -9,6 +9,36 @@ class SignInPage extends StatelessWidget {
         elevation: 2.0,
         centerTitle: true,
       ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
